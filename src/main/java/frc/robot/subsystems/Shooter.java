a0b2b4c52729c982892a6e51079e4107b6e814a5
KianJRoss <kianjamesross@gmail.com>
2024-03-12 16:39:41 -0400

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+  /** Creates a new Shooter. */
+  public Shooter() {}
+  private final PWMVictorSPX shooterMotor = new PWMVictorSPX(1);
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  public void shooterOn(){
+    shooterMotor.set(1);
+
+  }
+  public void shooterOff(){
+    shooterMotor.set(0);
+  }
+    public void shooterAmp(){
+    shooterMotor.set(.4);
+  } 
+  
+}
